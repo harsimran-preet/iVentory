945ffd328a8d81e3a8d0439782a571641342c32b
harsimran-preet <59977147+harsimran-preet@users.noreply.github.com>
2022-02-14 18:04:24 -0800

    to implement error handling

--- backend/backend.js
+++ backend/backend.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const aport = 3000;
 const users = require("./data/notes");
+const userRoutes = require('./routes/userRoutes');
 const dotenv = require("dotenv"); //place to store private stuff
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
 dotenv.config();
-
 connectDB();
-
+app.use(express.json()); 
 
 app.get("/", (req, res) => {
   res.send("API is running..");
@@ -21,7 +23,11 @@ app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.get("/users/:id", (req, res) => {
-  const user = users.find((n) => n.id === req.params.id);
-  res.send(user);
-});
+// app.get("/users/:id", (req, res) => {
+//   const user = users.find((n) => n.id === req.params.id);
+//   res.send(user);
+// });
+
+app.use("api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler); 
